d242a34404fe3bcb9fe82058f45590c870a6a35b
Thomas <64191507+tomikaso@users.noreply.github.com>
2023-11-20 08:51:09 +0100

    Delete sunrise.py
    Clean up

--- sunrise.py
+++
@@ -1,9 +0,0 @@
-#API-Key: XWX5XJW7E2Q9AGY1HOAMF2H15KZF5PLJ
-# Standort 3452377
-
-
-import requests
-
-response = requests.get("https://monitoringapi.solaredge.com/site/3452377/currentPowerFlow.json?api_key=XWX5XJW7E2Q9AGY1HOAMF2H15KZF5PLJ")
-print(response.status_code)
-print(response.json())
